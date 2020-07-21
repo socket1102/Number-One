f015e2576195a7d5e5c0a175f835af1d9b6776dc
Wangxu <504277326@qq.com>
2020-07-21 16:48:37 +0800

    试一下

---
+++ jeecg-boot-module-system/src/main/java/org/jeecg/modules/test/Test.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.test;
+
+/**
+ * @ClassName Test
+ * @Author asus
+ * @Date 2020/7/21 16:47
+ * @Version 1.0
+ * @Function <功能描述>
+ */
+public class Test {
+    public void index(){
+        System.out.println("123");
+    }
+}
