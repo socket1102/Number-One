da1b0e4b8cbf05822d2eefd004a44676beaef680
Wangxu <504277326@qq.com>
2020-07-21 17:01:31 +0800

    试一下

---
+++ jeecg-boot-module-system/src/main/java/org/jeecg/modules/voice/Voice.java
@@ -0,0 +1,36 @@
+package org.jeecg.modules.voice;
+
+import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.Dispatch;
+import com.jacob.com.Variant;
+
+import java.util.Scanner;
+
+/**
+ * @ClassName Voice
+ * @Author asus
+ * @Date 2020/7/21 16:58
+ * @Version 1.0
+ * @Function <功能描述>
+ */
+public class Voice {
+    public void speak(String args) {
+        ActiveXComponent sap = new ActiveXComponent("Sapi.SpVoice");
+        Dispatch sapo = sap.getObject();
+        try {
+            // 音量 0-100
+            sap.setProperty("Volume", new Variant(100));
+            // 语音朗读速度 -10 到 +10
+            sap.setProperty("Rate", new Variant(2));
+            Scanner scan = new Scanner(System.in);
+            String str = "体温过高，请前往医院检查";
+            // 执行朗读
+            Dispatch.call(sapo, "Speak", new Variant(args));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sapo.safeRelease();
+            sap.safeRelease();
+        }
+    }
+}
